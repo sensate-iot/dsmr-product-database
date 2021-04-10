659d211e27b2feb879631b1bcc9e2d5f1f387ca8
Michel Megens <michel@michelmegens.net>
2021-04-10 11:57:53 +0200

    auth: implement user and device authorization
    Implement the ability to authorize users and devices basaed on user
    ID's.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

---
+++ dbo/Stored Procedures/DsmrApi_SelectDevicesForUser.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[DsmrApi_SelectDevicesForUser]
+	@userId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SELECT [Id]
+		  ,[OnboardingToken]
+		  ,[ServiceName]
+		  ,[Enabled]
+		  ,[PowerSensorId]
+		  ,[GasSensorId]
+		  ,[EnvironmentSensorId]
+		  ,[Timestamp]
+	FROM [dbo].[Devices] d
+	INNER JOIN [dbo].[UserDevices] ud ON [ud].[DeviceId] = [d].[Id]
+	WHERE [ud].[UserId] = @userId
+END
---
+++ dbo/Stored Procedures/DsmrApi_SelectUserById.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[DsmrApi_SelectUserById]
+	@userId UNIQUEIDENTIFIER
+AS
+BEGIN
+	SELECT TOP (1) [Id]
+	  ,[CustomerId]
+	  ,[FirstName]
+	  ,[LastName]
+	  ,[Email]
+	  ,[Enabled]
+	  ,[Timestamp]
+	FROM [dbo].[Users] u
+	WHERE [u].[Id] = @userId
+END
