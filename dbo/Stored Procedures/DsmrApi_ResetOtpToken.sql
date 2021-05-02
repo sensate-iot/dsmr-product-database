2b5f84f9d98ec16890d859a444dafee6e5abc48e
Michel Megens <michel@michelmegens.net>
2021-05-02 12:21:39 +0200

    api: add OTP generation
    Implement the ability to generate OTP tokens.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- dbo/Stored Procedures/DsmrApi_ResetOtpToken.sql
+++ dbo/Stored Procedures/DsmrApi_ResetOtpToken.sql
@@ -1,1 +1,17 @@
-﻿
+﻿CREATE PROCEDURE [DsmrApi_ResetOtpToken]
+	@email NVARCHAR(64) 
+AS
+BEGIN
+	DECLARE @token UNIQUEIDENTIFIER;
+	SET @token = NEWID();
+
+	UPDATE [dbo].[Users]
+	SET [OnboardingToken] = @token
+	WHERE [Email] = @email
+
+	IF @@ROWCOUNT = 1
+	BEGIN
+		SELECT   @email AS [Email]
+				,@token AS [Token]
+	END
+END
