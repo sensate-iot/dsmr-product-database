63b9aec1d3947f09773267d3742613068eaa1c38
Michel Megens <michel@michelmegens.net>
2021-05-02 20:16:06 +0200

    otp: get the most recent phone number
    Get the most recent phone number when updating an OTP.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- dbo/Stored Procedures/DsmrApi_ResetOtpToken.sql
+++ dbo/Stored Procedures/DsmrApi_ResetOtpToken.sql
@@ -3,13 +3,23 @@
 	@token NVARCHAR(40)
 AS
 BEGIN
+	DECLARE @msisdn BIGINT
+
+	BEGIN TRAN [t1]
+
+	SELECT @msisdn = [Msisdn]
+	FROM [dbo].[Users]
+	WHERE [Email] = @email
+
 	UPDATE [dbo].[Users]
 	SET [OnboardingToken] = @token
 	WHERE [Email] = @email
 
+	COMMIT TRAN [t1]
+
 	IF @@ROWCOUNT = 1
 	BEGIN
-		SELECT   @email AS [Email]
+		SELECT   @msisdn AS [Email]
 				,@token AS [Token]
 	END
 END
